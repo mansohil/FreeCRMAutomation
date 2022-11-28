aaae0f3965c8cb80d581f587ae2b4cf5b2d6e397
mansohil <92969879+mansohil@users.noreply.github.com>
2022-11-28 10:22:56 +0530

    sohil--correcting log statements

--- src/main/java/Handling_Alerts_ConfirmBox_TestBase/TestBase.java
+++ src/main/java/Handling_Alerts_ConfirmBox_TestBase/TestBase.java
@@ -10,50 +10,55 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.events.EventFiringWebDriver;
 
+import Handling_Alerts_ConfirmBox_TestUtilities.WebEventListen;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-public class TestBase {
-	
-	public static WebDriver driver;
-   public static Properties prop;
-   public static Logger logger;
-   
-   
-	public TestBase() throws IOException {
-		
-		prop=new Properties(); 
-	    FileInputStream file=new FileInputStream("C:\\Users\\mdsohil\\eclipse-workspace\\Alert_Handling\\src\\main\\java\\Handling_Alerts_ConfirmBox_Properties\\config.properties");
-	    prop.load(file);
-		 
-	  
-}
-
-
 @SuppressWarnings("deprecation")
-public static void initialization() {
-	if(prop.getProperty("browser").equals("chrome")) {
-		 WebDriverManager.chromedriver().setup();
-		 driver=new ChromeDriver();
+public class TestBase {
+
+	public static WebDriver driver;
+	public static Properties prop;
+	public static Logger logger;
+	static EventFiringWebDriver e_driver;
+	WebEventListen eventListner;
+
+	public TestBase() throws IOException {
+
+		prop = new Properties();
+		FileInputStream file = new FileInputStream(
+				"C:\\Users\\mdsohil\\eclipse-workspace\\Alert_Handling\\src\\main\\java\\Handling_Alerts_ConfirmBox_Properties\\config.properties");
+		prop.load(file);
+
 	}
-	
-	else if(prop.getProperty("browser").equals("firefox")) {
-		 WebDriverManager.firefoxdriver().setup();
-		 driver=new FirefoxDriver();
+
+	public static void initialization() throws IOException {
+		if (prop.getProperty("browser").equals("chrome")) {
+			WebDriverManager.chromedriver().setup();
+			driver = new ChromeDriver();
+		}
+
+		else if (prop.getProperty("browser").equals("firefox")) {
+			WebDriverManager.firefoxdriver().setup();
+			driver = new FirefoxDriver();
+		}
+
+		else if (prop.getProperty("browser").equals("edge")) {
+			WebDriverManager.edgedriver().setup();
+			driver = new EdgeDriver();
+		}
+
+		e_driver = new EventFiringWebDriver(driver);
+		WebEventListen eventListner = new WebEventListen();
+		e_driver.register(eventListner);
+		driver = e_driver;
+
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		driver.manage().deleteAllCookies();
+		driver.get(prop.getProperty("url"));
+
 	}
-	
-	else if(prop.getProperty("browser").equals("edge")) {
-		 WebDriverManager.edgedriver().setup();
-		 driver=new EdgeDriver();
-	}
-	
-	 driver.manage().window().maximize();
-	 driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-	 driver.manage().deleteAllCookies();
-	 driver.get(prop.getProperty("url"));
-	  
-	 
-	 
-}
 
 }
